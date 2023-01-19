a3ce66e0fb8792d46604e8a45c79a7e6155d791a
Mayuri2727 <mayuriasabe107@gmail.com>
2023-01-19 13:30:21 +0530

    Calculate Daily Empolyee Wage

--- EmpWage/EmpAttendance.java
+++ EmpWage/EmpAttendance.java
@@ -4,14 +4,27 @@ public class EmpAttendance {
     public static void main(String[] args) {
         System.out.println("Welcome to Employee Wage Computation Program");
         int Emp_Full_Time = 1;
-        double Attendance = Math.floor(Math.random() *10)%2;
-        System.out.println("Attendance of emp: " +Attendance);
-        if (Attendance == Emp_Full_Time) {
-            System.out.println("Employee is Present");
+        int Emp_Rate_Hour = 20;
+        int empHrs = 0;
+        int empWage = 1;
+        double empCheck = Math.floor(Math.random() *10)%2;      //RANDOM Method for EMP Present or Absent
+        System.out.println("Attendance of emp: " +empCheck);
+
+        if (empCheck == Emp_Full_Time) {                       //Check Presence & Emp Hours
+            empHrs = 8;
+
         } else {
-            System.out.println("Employee is Absent");
+            empHrs = 0;
+        }
+        empWage = empHrs * Emp_Rate_Hour;                      // Calculate EMP Wage
+        System.out.println("Employee Wage : " +empWage);
         }
         }
-    }
+
+
+
+
+
+
 
 
