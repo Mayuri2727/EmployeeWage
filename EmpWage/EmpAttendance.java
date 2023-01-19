801192e62230c6604c0717d4effcd1705b8ef986
Mayuri2727 <mayuriasabe107@gmail.com>
2023-01-19 14:05:22 +0530

    Add Part Time Employee and Wage

--- EmpWage/EmpAttendance.java
+++ EmpWage/EmpAttendance.java
@@ -3,14 +3,14 @@ package EmpWage;
 public class EmpAttendance {
     public static void main(String[] args) {
         System.out.println("Welcome to Employee Wage Computation Program");
-        int Emp_Full_Time = 1;
+        int Emp_Part_Time = 1;
         int Emp_Rate_Hour = 20;
         int empHrs = 0;
-        int empWage = 1;
+        int empWage = 0;
         double empCheck = Math.floor(Math.random() *10)%2;      //RANDOM Method for EMP Present or Absent
         System.out.println("Attendance of emp: " +empCheck);
 
-        if (empCheck == Emp_Full_Time) {                       //Check Presence & Emp Hours
+        if (empCheck == Emp_Part_Time) {                       //Check Presence & Emp Hours
             empHrs = 8;
 
         } else {
