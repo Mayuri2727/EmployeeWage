a2d0410882ee6648cbc32f7528dec935cd62b46f
Mayuri2727 <mayuriasabe107@gmail.com>
2023-01-19 15:16:28 +0530

    Calculating Employee Wages  month

--- EmpWage/EmpAttendance.java
+++ EmpWage/EmpAttendance.java
@@ -5,13 +5,15 @@ public class EmpAttendance {
     public static final int Emp_Full_Time = 2;
     public static final int Emp_Rate_Hour = 20;
     public static final int Emp_Work_Day = 2;
-
+    public static final int Max_Hrs_In_Month = 10;
     public static void main(String[] args) {
         // System.out.println("Welcome to Employee Wage Computation Program");
         int empHrs = 0;
-        int empWage = 0;
-        int totalEmpWage = 0;
-        for (int day = 0; day < Emp_Work_Day; day++) {
+        //int empWage = 0;
+        int totalEmpDays = 0;
+        int totalEmpHrs = 0;
+        while (totalEmpHrs <= Max_Hrs_In_Month && totalEmpDays < Emp_Work_Day ) {
+            totalEmpDays++;
             int empCheck = (int) Math.floor(Math.random() + 10) % 3;
             switch (empCheck) {                         //Switch Case
                 case Emp_Part_Time:
@@ -24,11 +26,11 @@ public class EmpAttendance {
                     empHrs = 0;
             }
 
-            empWage = empHrs * Emp_Rate_Hour;
-            totalEmpWage += empWage;
-            System.out.println("Employee Wage : " + empWage);
+            totalEmpHrs += empHrs;
+            System.out.println("Day#: " + totalEmpDays + ",  Emp hr: " + empHrs);
         }
-        System.out.println("Total Employee Wage : " +totalEmpWage);
+        int totalEmpWage = totalEmpHrs * Emp_Rate_Hour;
+        System.out.println("Total Emp Wage: " +totalEmpWage );
     }
 }
 
